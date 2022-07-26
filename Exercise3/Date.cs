9784e9407d4b7a8b161604a718ec874d43f9caf0
radusalex1 <54530468+radusalex1@users.noreply.github.com>
2022-07-26 15:11:44 +0300

    First part of ex3.

--- Exercise3/Date.cs
+++ Exercise3/Date.cs
@@ -3,15 +3,30 @@
     public class Date
     {
         public DateTime CalendaristicDate;
+        public DateTime CurrentDate = DateTime.Today;
 
         public Date(DateTime calendaristicDate)
         {
             CalendaristicDate = calendaristicDate;
         }
-
-        public string IsValid()
+        public Date()
         {
-            return string.Empty;
+
+        }
+
+        public bool IsValid(int year,int month,int day)
+        {
+            try
+            {
+                var date = new DateTime(year, month, day);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+           
+            return true;
         }
 
         public bool IsExpired()
