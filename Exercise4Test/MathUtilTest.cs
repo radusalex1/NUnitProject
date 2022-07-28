d990f9135ab43245570c249721aef623b05f9cea
oanavisoiu <oanavisoiu@gmail.com>
2022-07-28 11:56:54 +0300

    Ex4

--- Exercise4Test/MathUtilTest.cs
+++ Exercise4Test/MathUtilTest.cs
@@ -1,10 +1,42 @@
-﻿
+﻿using Exercise4;
+using NUnit.Framework;
+
 namespace Exercise4Test
 {
-    //TODO: daca rezultatul este corect;
-    //TODO: test daca se face corect double cast; 2==2.00 pass && 2!=2.01 pass
+    [TestFixture]
     public class MathUtilTest
     {
+        [Test]
+        [TestCase(1,2,ExpectedResult =1.5)]
+        [TestCase(23,27, ExpectedResult =25)]
+        public double Average_ShouldReturnAverage_WhenInputIsValid(int a,int b)
+        {
+            MathUtil math = new MathUtil(a,b);
+
+            return math.Average();
+        }
+
+        [Test]
+        [TestCase(1,ExpectedResult =1.0)]
+        [TestCase(24, ExpectedResult = 24.0)]
+        [TestCase(-34, ExpectedResult = -34.0)]
+        public double ConvertToDouble_ShouldRetrunNumberAsDouble_WhenInputIsValid(int a)
+        {
+            MathUtil math = new MathUtil();
+
+            return math.ConvertToDouble(a);
+        }
+
+        [Test]
+        [TestCase(1.1,4.5, ExpectedResult =5.6)]
+        [TestCase(21, 14, ExpectedResult = 35)]
+        [TestCase(-64,-4, ExpectedResult = -68)]
+        public double GetSum_ShouldReturnSumOfDoubles_WhenInputIsValid(double x, double y)
+        {
+            MathUtil math = new MathUtil();
+
+            return math.GetSum(x, y);
+        }
 
     }
 }
